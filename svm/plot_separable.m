84fc247c24c968a0d209f51603a70bb1829b15f3
CarlosCobas <carlos.cobas1997@gmail.com>
2021-01-10 19:45:24 +0100

    Fronteras pintadas

--- svm/plot_separable.m
+++ svm/plot_separable.m
@@ -11,9 +11,18 @@ x1 = [0:7]
 x2 = -theta(1)/theta(2)*x1 - theta0/theta(2)
 
 SV = X(res.sv_indices,:)
+SVl = xl(res.sv_indices,:)
 
+
+margin_total = 2/norm(theta)
+margin = 1/norm(theta(2))
 
 hf = figure();
-plot(X(xl==1,1),X(xl==1,2),"sr",X(xl==2,1),X(xl==2,2),"sb",x1,x2,"-k",SV(:,1),SV(:,2),"+r")
+
+plot(X(xl==1,1),X(xl==1,2),"sr",X(xl==2,1),X(xl==2,2),"sb",x1,x2,"-k",SV(:,1),SV(:,2),"+r", x1, x2+margin, "-g", x1, x2-margin, "-b")
+grid on
+text (0.5, 6.5, char(["marg = " , num2str(margin_total)]));
+axis([0 7 0 7])
+
 set (hf, "visible", "off");
 print(hf, "plot15_7.jpg", "-djpg");
