a945615461233dd7e28c0ed87a383eba9eb42f81
CarlosCobas <carlos.cobas1997@gmail.com>
2021-01-11 10:37:13 +0100

    Terminado el ejercicio 2.4. De Mistura de Gaussianas queda solo la memoria

--- mix_gaus/pca+mixgaussian-eva.m
+++ mix_gaus/pca+mixgaussian-eva.m
@@ -1,7 +1,7 @@
 #!/usr/bin/octave -qf
 
-if (nargin!=6)
-printf("Usage: pca+mixgaussian-eva.m <trdata> <trlabs> <tedata> <telabels> <ks> <alphas>\n")
+if (nargin!=7)
+printf("Usage: pca+mixgaussian-eva.m <trdata> <trlabs> <tedata> <telabels> <pcaKs> <ks> <alphas>\n")
 exit(1);
 end;
 
@@ -10,8 +10,10 @@ trdata=arg_list{1};
 trlabs=arg_list{2};
 tedata=arg_list{3};
 telabs=arg_list{4};
-ks=str2num(arg_list{5});
-alphas=str2num(arg_list{6});
+pcaKs=str2num(arg_list{5});
+Ks=str2num(arg_list{6});
+alphas=str2num(arg_list{7});
+
 
 load(trdata);
 load(trlabs);
@@ -19,20 +21,20 @@ load(tedata);
 load(telabs);
 
 
-[m W] = pca(Xtr);
+[m W] = pca(X);
 
-Xtr = Xtr - m;
-Xdv = Xdv - m;
+Xr = X - m;
+Yr = Y - m;
 
 err_mat = [];
 
 for i=1:length(alphas)
 	err_mat_a = [];
 	for k=1:length(pcaKs)
-		pcaXtr = Xtr * W(:,1:pcaKs(k));
-		pcaXdv = Xdv * W(:,1:pcaKs(k));
+		pcaXr = Xr * W(:,1:pcaKs(k));
+		pcaYr = Yr * W(:,1:pcaKs(k));
 		for j=1:length(Ks)
-			edv = mixgaussian(pcaXtr,xltr,pcaXdv,xldv,Ks(j),alphas(i));
+			edv = mixgaussian(pcaXr,xl,pcaYr,yl,Ks(j),alphas(i));
 			printf("\n  alpha   PCA    K   dv-err");
 			printf("\n  -----   ---   ---  ------\n");
 			printf("  %.1e %3d  %3d  %6.3f\n\n",alphas(i),pcaKs(k),Ks(j),edv);
