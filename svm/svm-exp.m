d7829f2bf940dbe881dc1e16f4ae0b00048dc736
CarlosCobas <carlos.cobas1997@gmail.com>
2021-01-11 16:36:13 +0100

    Creado svm-exp.m

---
+++ svm/svm-exp.m
@@ -0,0 +1,71 @@
+#!/usr/bin/octave -qf
+addpath("matlab");
+if (nargin!=7)
+printf("Usage: pca+mixgaussian-exp.m <trdata> <trlabels> <C> <T> <D> <%%trper> <%%dvper>\n")
+exit(1);
+end;
+
+arg_list=argv();
+trdata=arg_list{1};
+trlabs=arg_list{2};
+C=str2num(arg_list{3});
+T=str2num(arg_list{4});
+D=str2num(arg_list{5});
+trper=str2num(arg_list{6});
+dvper=str2num(arg_list{7});
+
+load(trdata);
+load(trlabs);
+
+N=rows(X);
+seed=23; rand("seed",seed); permutation=randperm(N);
+X=X(permutation,:); xl=xl(permutation,:);
+
+Ntr=round(trper/100*N);
+Ndv=round(dvper/100*N);
+Xtr=X(1:Ntr,:); xltr=xl(1:Ntr);
+Xdv=X(N-Ndv+1:N,:); xldv=xl(N-Ndv+1:N);
+
+
+% [m W] = pca(Xtr);
+
+% Xtr = Xtr - m;
+% Xdv = Xdv - m;
+
+err_mat = [];
+
+for t=1:length(T)
+	err_mat_a = [];
+	for c=1:length(C)
+		% pcaXtr = Xtr * W(:,1:pcaKs(k));
+		% pcaXdv = Xdv * W(:,1:pcaKs(k));
+		if(T(t) == 1)
+            for d=1:length(D)
+
+                args = char(["-t " , num2str(T(t)), "-c " , num2str(C(c)), "-d " ,num2str(D(d))])
+                res = svmtrain(xl, X, args);
+
+                % edv = mixgaussian(pcaXtr,xltr,pcaXdv,xldv,Ks(j),alphas(i));
+                % printf("\n  alpha   PCA    K   dv-err");
+                % printf("\n  -----   ---   ---  ------\n");
+                % printf("  %.1e %3d  %3d  %6.3f\n\n",alphas(i),pcaKs(k),Ks(j),edv);
+                
+                edv = mean(xldv!=cls)*100;
+                err_mat_a=[err_mat_a; T(t),C(c),D(d),edv];
+            
+            end
+        else
+            args = char(["-t " , num2str(T(t)), "-c " , num2str(C(c))])
+            res = svmtrain(xl, X, args);
+
+            edv = mean(xldv!=cls)*100;
+            err_mat_a=[err_mat_a;  T(t),C(c),0,edv];
+        end
+		
+	end
+
+	err_mat = [err_mat, err_mat_a];
+end
+
+save_precision(4); 
+save("error_pca+mixgaussian-exp.out", "err_mat");
